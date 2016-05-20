11056e482691f5e86c6ae1db849653c0b6ede593
Amith Kumar Aellanki <amith@amiths-macbook-pro.local>
2016-05-19 20:49:43 -0400

    Initial Commit

---
+++ Geocalculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Geocalculator
+//
+//  Created by Amith Kumar Aellanki on 5/19/16.
+//  Copyright © 2016 Amith Kumar Aellanki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Geocalculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Geocalculator
+//
+//  Created by Amith Kumar Aellanki on 5/19/16.
+//  Copyright © 2016 Amith Kumar Aellanki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
