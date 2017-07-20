45baccbef99d531dc36d1053166ac5ea49c8ba63
Amith Kumar Aellanki <amith@Amiths-MacBook-Pro.local>
2017-07-20 17:30:34 -0400

    commit

--- Geocalculator/FirstViewController.swift
+++ Geocalculator/FirstViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import CoreLocation
+import Foundation
+
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var Enterlatitude1: DecimalMinusTextField!
+    
+    
+    @IBOutlet weak var Enterlatitude2: DecimalMinusTextField!
+    
+    
+    @IBOutlet weak var EnterLongitude1: DecimalMinusTextField!
+
+    
+    @IBOutlet weak var EnterLongitude2: DecimalMinusTextField!
+    
+    @IBOutlet weak var distresult: UILabel!
+    
+    
+    @IBOutlet weak var bearresult: UILabel!
+    
+    @IBAction func Tocalculate(sender: AnyObject) {
+        let lat1 :CLLocationDegrees = Double(self.Enterlatitude1.text!)!
+        let long1 :CLLocationDegrees = Double(self.EnterLongitude1.text!)!
+        let lat2 :CLLocationDegrees = Double(self.Enterlatitude2.text!)!
+        let long2 :CLLocationDegrees = Double(self.EnterLongitude2.text!)!
+        
+        let p1 = CLLocation(latitude: lat1, longitude: long1)
+        let p2 = CLLocation(latitude: lat2, longitude: long2)
+        
+        let a = p1.distanceFromLocation(p2)
+        let b = p1.bearingToPoint(p2)
+        
+        self.distresult.text = "\(Double(round(100*a)/100)/1000) Kilometers"
+        self.bearresult.text = "\(Double(round(100*b)/100)) Degrees"
+        
+        
+    }
+    
+    @IBAction func Toclear(sender: AnyObject) {
+        view.endEditing(true)
+        
+        self.Enterlatitude1.text = " "
+        
+        self.EnterLongitude1.text = " "
+        
+        self.Enterlatitude2.text = " "
+        
+        self.EnterLongitude2.text = " "
+        
+        self.distresult.text = " "
+        
+        self.bearresult.text = " "
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
--- Geocalculator/SecondViewController.swift
+++ Geocalculator/SecondViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import CoreLocation
+import Foundation
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var Seclatitude: DecimalMinusTextField!
+    
+    @IBOutlet weak var Seclongtide: DecimalMinusTextField!
+    
+    @IBOutlet weak var SecDistance: DecimalMinusTextField!
+    
+    @IBOutlet weak var SecBearing: DecimalMinusTextField!
+
+    @IBOutlet weak var SecResult: UILabel!
+    
+    @IBAction func SecCalculate(sender: AnyObject) {
+        
+        let Seclat :CLLocationDegrees = Double(self.Seclatitude.text!)!
+        let Seclong :CLLocationDegrees = Double(self.Seclongtide.text!)!
+        let Secdist :CLLocationDegrees = Double(self.SecDistance.text!)!
+        let Secbear :CLLocationDegrees = Double(self.SecBearing.text!)!
+    
+        let p = CLLocation(latitude: Seclat, longitude: Seclong)
+        
+        let SecResult :CLLocation = p.findingPoint(Double(self.SecBearing.text!)!, atDistance: Double(self.SecDistance.text!)!)
+        
+        self.SecResult.text = "(\(Double(round(SecResult.coordinate.latitude*100000)/100000)), \(Double(round(SecResult.coordinate.longitude*100000)/100000))"
+        
+        }
+    
+    @IBAction func SecClear(sender: AnyObject) {
+        view.endEditing(true)
+        self.Seclatitude.text = " "
+        self.Seclongtide.text = " "
+        self.SecDistance.text = " "
+        self.SecBearing.text = " "
+        self.SecResult.text = " "
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
